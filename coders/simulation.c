695861df9e434b1421493456163ea2a3766c45fd
Mart1nlo <martinlorenzo0802@gmail.com>
2026-03-21 19:13:16 +0100

    Basics of logic added

--- coders/simulation.c
+++ coders/simulation.c
@@ -6,7 +6,7 @@
 /*   By: martin <martin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/21 16:19:18 by martin            #+#    #+#             */
-/*   Updated: 2026/03/21 17:34:51 by martin           ###   ########.fr       */
+/*   Updated: 2026/03/21 19:12:02 by martin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,48 @@ void	print_env(t_env *e)
 	printf("-----------------\n");
 }
 
-int	launch_simulation(struct s_env *env)
+void	*coder_routine(void *arg)
 {
-	print_env(env);
+	t_coder	*coder;
+	t_env	*env;
+
+	coder = (t_coder *)arg;
+	env = coder->env;
+	while (1)
+	{
+		/*
+		1. Verifier si on doit s'arreter (Burnout ou Finish)
+		2. Refactor (penser)
+		3. Prendre les dongles
+		4. Compiler
+		5. Relacher les dongles
+		Pour l'instant on met un break pour ne pas faire de boucle infinie
+		*/
+		break ;
+	}
+	return (NULL);
+}
+
+int	launch_simulation(t_env *env)
+{
+	int	i;
+
+	env->start_time = get_time();
+	i = 0;
+	while (i < env->nb_coders)
+	{
+		if (pthread_create(&env->coders[i].thread, NULL,
+				&coder_routine, &env->coders[i]) != 0)
+		{
+			return (1);
+		}
+		i++;
+	}
+	i = 0;
+	while (i < env->nb_coders)
+	{
+		pthread_join(env->coders[i].thread, NULL);
+		i++;
+	}
 	return (0);
 }
